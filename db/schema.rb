518f43a42622d218f12c19d0949bbb0f4d692636
emesc <tahder77@gmail.com>
2017-03-04 00:08:49 +1100

    Added hmt relationship between programs and school levels.
    Generated suitabilities model and migrations for this relationship.
    Edited the seed file for programs.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170303050056) do
+ActiveRecord::Schema.define(version: 20170303062801) do
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
@@ -55,6 +55,14 @@ ActiveRecord::Schema.define(version: 20170303050056) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "suitabilities", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "program_id"
+    t.integer  "school_level_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.index ["program_id", "school_level_id"], name: "index_suitabilities_on_program_id_and_school_level_id", using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
