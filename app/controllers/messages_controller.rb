02cd846d969320f38d9a4d0a4d6a2c370ef82171
emesc <tahder77@gmail.com>
2017-03-05 16:25:29 +1100

    Created initial contact form.

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,17 @@
+class MessagesController < ApplicationController
+  def new
+    @categories = Category.all
+    @school_levels = SchoolLevel.all
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+  end
+
+  private
+
+    def message_params
+      params.require(:message).permit(:name, :email, :subject, :content)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  root "programs#index"
+  get "contact", to: "messages#new", as: "contact"
+  post "contact", to: "messages#create"
 
   resources :programs, only: [:index, :show]
   resources :categories, only: [:index, :show]
+
+  root "programs#index"
 end
