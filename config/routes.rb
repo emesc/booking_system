2a5c9e883c1b57a617badaf639ecbd874974149e
emesc <tahder77@gmail.com>
2017-03-06 20:18:54 +1100

    Created school levels actions and views for admin.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   namespace :admin do
-    resources :programs, :categories
+    resources :programs, :categories, :school_levels
   end
 
   get "contact", to: "messages#new", as: "contact"
