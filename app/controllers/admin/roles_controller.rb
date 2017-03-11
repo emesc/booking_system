f8310e71931026e7aa8159876526568933efb0b2
emesc <tahder77@gmail.com>
2017-03-11 19:45:14 +1100

    Generated role controller and show view.
    Updated back to previous page links.
    Updated stylesheet.

---
+++ app/controllers/admin/roles_controller.rb
@@ -0,0 +1,12 @@
+class Admin::RolesController < Admin::ApplicationController
+
+  def index
+    @categories = Category.all
+    @school_levels = SchoolLevel.all
+    @roles = Role.all
+  end
+
+  def show
+    @role = Role.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: "admin/sessions" }
 
   namespace :admin do
-    resources :programs, :categories, :school_levels, :users
+    resources :programs, :categories, :school_levels, :users, :roles
   end
 
   get "contact", to: "messages#new", as: "contact"
