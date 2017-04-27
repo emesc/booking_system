2e5c62f6cc2c574077688a0b6bfeaef968c37b34
emesc <tahder77@gmail.com>
2017-04-27 13:08:50 +1000

    Update spec to test for admins only.

--- spec/features/admin_signing_up_users_spec.rb
+++ spec/features/admin_signing_up_users_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.feature "Signing up a user" do
+RSpec.feature "Admin signing up a user" do
 
   # only admin & managers can sign up a user
   before do
@@ -8,8 +8,8 @@ RSpec.feature "Signing up a user" do
     r2 = Role.create!(name: "Manager", description: "Can read items. Can create or delete user/teacher accounts")
     r3 = Role.create!(name: "Regular", description: "Can read items")
     @admin = User.create!(first_name: "Admin", last_name: "Doe", email: "admindoe@example.com", password: "foobar", role_id: r1.id)
-    @manager = User.create!(first_name: "Manager", last_name: "Doe", email: "managerdoe@example.com", password: "foobar", role_id: r2.id)
-    @regular = User.create!(first_name: "Regular", last_name: "Doe", email: "johndoe@example.com", password: "foobar", role_id: r3.id)
+    # @manager = User.create!(first_name: "Manager", last_name: "Doe", email: "managerdoe@example.com", password: "foobar", role_id: r2.id)
+    # @regular = User.create!(first_name: "Regular", last_name: "Doe", email: "johndoe@example.com", password: "foobar", role_id: r3.id)
     c = Category.create!(name: "Mathematics")
     s1 = SchoolLevel.create!(name: "K-2")
     s2 = SchoolLevel.create!(name: "3-4")
@@ -18,7 +18,7 @@ RSpec.feature "Signing up a user" do
     login_as @admin
   end
 
-  scenario "by an admin with valid credentials" do
+  scenario "with valid credentials" do
     visit "/"
     expect(page).to have_content("Program Booking")
     click_link "Welcome, #{@admin.first_name} #{@admin.last_name}"
@@ -38,9 +38,27 @@ RSpec.feature "Signing up a user" do
 
     expect(current_path).to eq admin_users_path
     expect(page).to have_content("User successfully created")
+    expect(User.count).to eq 2
   end
 
   scenario "with invalid credentials" do
+    visit "/"
+    click_link "Welcome, #{@admin.first_name} #{@admin.last_name}"
+    click_link "Admin Area"
 
+    expect(current_path).to eq admin_programs_path
+    click_link "Users"
+    click_link "Create new"
+
+    expect(current_path).to eq new_admin_user_path
+    fill_in "First name", with: ""
+    fill_in "Last name", with: ""
+    select "Regular", from: "user_role_id"
+    fill_in "Email", with: ""
+    fill_in "Password", with: ""
+    click_button "Create user"  
+
+    expect(page).to have_content("errors")
+    expect(User.count).to eq 1
   end
 end
