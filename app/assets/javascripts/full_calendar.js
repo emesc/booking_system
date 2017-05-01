d87601a49371b0298f0df68627b7a22951c7560f
emesc <tahder77@gmail.com>
2017-05-01 18:42:28 +1000

    Added an initial js file for full_calendar.

---
+++ app/assets/javascripts/full_calendar.js
@@ -0,0 +1,12 @@
+var initialise_calendar;
+initialise_calendar = function() {
+  // loop thru each one of the calendar classes
+  $('.calendar').each(function() {
+    // set calendar var to $this bec we're going to need to use the calendar var within the callbacks to ref back to the calendar obj once we get further along in here; easier to do this now
+    var calendar = $(this);
+    // call fullCalendar on the current calendar obj
+    calendar.fullCalendar({});
+  });
+};
+
+$(document).on('turbolinks:load', initialise_calendar);
