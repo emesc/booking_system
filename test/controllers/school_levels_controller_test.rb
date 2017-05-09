d5cfc50f7b0ca98c808eeabbc1db3a86c78c6bc4
emesc <tahder77@gmail.com>
2017-05-09 18:27:29 +1000

    Removed auto generated controller tests.

--- test/controllers/school_levels_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class SchoolLevelsControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get school_levels_show_url
-    assert_response :success
-  end
-
-end
